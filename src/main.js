7e1479f50897286b301bb8e84d74433003dd6171
GAEBOLGwk <2183777854@qq.com>
2021-04-19 07:48:13 +0800

    master

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//挂载轮播图
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+// swiper/css/swiper.css
+import 'swiper/swiper-bundle.css'
+// import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper)
+
+//视频播放器
+import VideoPlayer from 'vue-video-player'
+import 'vue-video-player/src/custom-theme.css'
+import 'video.js/dist/video-js.css'
+Vue.use(VideoPlayer)
+
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,50 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'sign',
+    component: () => import('../views/sign.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/phone',
+    name: 'phone',
+    component: () => import('../views/phone.vue')
+  },
+  {
+    path: '/code',
+    name: 'code',
+    component: () => import('../views/code.vue')
+  },
+  {
+    path: '/me',
+    name: 'me',
+    component: () => import('../views/me.vue')
+  },
+  {
+    path: '/edit',
+    name: 'edit',
+    component: () => import('../views/edit.vue')
+  },
+  {
+    path: '/message',
+    name: 'message',
+    component: () => import('../views/message.vue')
+  },
+  {
+    path: '/index',
+    name: 'index',
+    component: () => import('../views/index.vue')
+  },
+  {
+    path: '/video',
+    name: 'video',
+    component: () => import('../views/videolist.vue')
+  },
+  
 ]
 
 const router = new VueRouter({
